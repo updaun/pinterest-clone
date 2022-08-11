ef1978407dce02d60e35804d483047ac508fcb1c
updaun <updauney@daum.net>
2022-08-11 17:47:49 +0900

    Detail view

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView
 
 app_name = "accountapp"
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='accountapp/logout.html'), name='logout'),  
 
     path('create/', AccountCreateView.as_view(), name='create'),    
+    path('detrail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 ]
