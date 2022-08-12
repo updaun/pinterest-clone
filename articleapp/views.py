bbd4cb7038b79a81afa45d2ee3d906c8f6a2e1c0
updaun <updauney@daum.net>
2022-08-12 13:04:02 +0900

    Article ListView and Pagination

--- articleapp/views.py
+++ articleapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from articleapp.decorators import article_ownership_required
 from django.contrib.auth.decorators import login_required
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 from django.urls import reverse, reverse_lazy
 
 from articleapp.forms import ArticleCreationForm
@@ -52,3 +52,8 @@ class ArticleDeleteView(DeleteView):
     template_name = 'articleapp/delete.html'
     success_url = reverse_lazy("articleapp:list")
     
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 1
