5a2d52e6c1d66aa79935e84e82366d26cf717413
updaun <updauney@daum.net>
2022-08-12 18:21:11 +0900

    Comment DetailView, DeleteView, Decorator

---
+++ commentapp/decorators.py
@@ -0,0 +1,10 @@
+from commentapp.models import Comment
+from django.http import HttpResponseForbidden
+
+def comment_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        comment = Comment.objects.get(pk=kwargs['pk'])
+        if not comment.writer == request.user:
+            return HttpResponseForbidden()
+        return func(request, *args, **kwargs)
+    return decorated
--- commentapp/views.py
+++ commentapp/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView
 from commentapp.forms import CommentCreationForm
 
 from django.urls import reverse
 
+from articleapp.models import Article
 from commentapp.models import Comment
+
+from django.utils.decorators import method_decorator
+from commentapp.decorators import comment_ownership_required
 
 class CommentCreateView(CreateView):
     model = Comment
@@ -19,6 +23,16 @@ class CommentCreateView(CreateView):
         temp_comment.writer = self.request.user
         temp_comment.save()
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
+
+@method_decorator(comment_ownership_required, 'get')
+@method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk':self.object.article.pk})
