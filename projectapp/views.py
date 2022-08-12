5d9814223bc639360397ac63c2f234e9a6079ab9
updaun <updauney@daum.net>
2022-08-13 01:51:25 +0900

    Projectapp CreateView, DetailView, ListView

---
+++ projectapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.views.generic import CreateView, DetailView, ListView
+
+# Create your views here.
+from projectapp.models import Project
+
+from projectapp.forms import ProjectCreationForm
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class ProjectCreateView(CreateView):
+    model = Project
+    form_class = ProjectCreationForm
+    template_name = 'projectapp/create.html'
+
+    def get_success_url(self):
+        return reverse('projectapp:detail', kwargs={'pk':self.object.pk})
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    context_object_name = 'target_project'
+    template_name = 'projectapp/detail.html'
+
+
+class ProjectListView(ListView):
+    model = Project
+    context_object_name = 'project_list'
+    template_name = 'projectapp/list.html'
+    paginate_by = 5
